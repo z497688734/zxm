d2393751134198f6f13b8752863e4e36e00a2972
497688734@qq.com <liuhaoyang@sfmail.sf-express.com>
2024-07-19 04:00:47 +0800

    add code

--- main5.py
+++ main5.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         "Referer": "https://sync.hangeshenzhou.com/",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     }
-    pageNum = 1840
+    pageNum = 1
     maxPageNum = 1846
     fileName = "order.csv"
     while True:
